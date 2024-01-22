89200e451d8968915b25b431112d713bce8d94d2
joaobug <joaoenfrance@gmail.com>
2024-01-22 13:08:26 -0300

    Initial Commit

---
+++ LivroDeReceitas/LivroDeReceitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivroDeReceitasApp.swift
+//  LivroDeReceitas
+//
+//  Created by Joao Leal on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LivroDeReceitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
